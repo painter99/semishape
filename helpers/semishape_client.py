99771b90d51e0692da7b9d87d6b70c1f4696edd2
mares.pa <mares.pa@protonmail.com>
2026-04-06 18:49:39 +0000

    fix: Replace removed jadro/ import with src.semishape in semishape_client.py

--- helpers/semishape_client.py
+++ helpers/semishape_client.py
@@ -25,8 +25,7 @@ PROJECT_ROOT = Path(__file__).parent.parent
 if str(PROJECT_ROOT) not in sys.path:
     sys.path.insert(0, str(PROJECT_ROOT))
 
-from jadro.hlavni import SemiShape, Vysledek
-from jadro.utils.metriky import get_metriky
+from src.semishape import SemiShape
 
 
 @dataclass
